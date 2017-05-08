f27d07630bc6b4d502098e2e0e0fd587c133368f
alexandre025 <alexandre.ferraille@gmail.com>
2017-05-08 16:42:04 +0200

    Generate home controller with example of basic test

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe HomeController, type: :controller do
+  render_views
+
+  describe 'GET index' do
+
+    it 'http status success' do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+  end
+
+end
