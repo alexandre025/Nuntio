41a30d504c26229d531b22f612ed32f104c2c0b5
alexandre025 <alexandre.ferraille@gmail.com>
2017-05-10 11:39:15 +0200

    Tests & factories

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Team, type: :model do
-  let(:team) { build(:team) }
 
   it 'can be saved' do
-    expect(team.save).to eq(true)
+    expect(build(:team).save).to eq(true)
   end
+
+  it 'create a new tenant' do
+    team = create(:team)
+    expect(Apartment.tenant_names).to include(team.code)
+  end
+
+  it 'and switch to this new tenant' do
+    team = create(:team)
+    expect(Apartment::Tenant.switch!(team.code))
+  end
+
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,8 @@ RSpec.configure do |config|
 
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.extend ControllerMacros, type: :controller
+
+  config.before(:each) do
+    @current_team = create(:team)
+  end
 end
