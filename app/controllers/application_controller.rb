50aeed9d05589b8eaf68d2505feb9bc1f72b9c9d
alexandre025 <alexandre.ferraille@gmail.com>
2017-05-11 10:43:39 +0200

    Protect from public tenancy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_team
+
+  def current_team
+    @current_team ||= Team.current
+  end
+
+  private
+
+  def require_team_tenancy
+    redirect_to root_path if Apartment::Tenant.current == 'public'
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
+  before_action :require_team_tenancy, only: [:dashboard]
+
   def index
+
+  end
+
+  def dashboard
+
   end
 end
