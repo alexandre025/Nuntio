d0981c85d0ecd9d07059e21812a36a9fb4c35e9d
alexandre025 <alexandre.ferraille@gmail.com>
2017-05-20 18:35:10 +0200

    Comments model & tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170520155927) do
+ActiveRecord::Schema.define(version: 20170520161701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,18 @@ ActiveRecord::Schema.define(version: 20170520155927) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["theme_id"], name: "index_categories_on_theme_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "commentable_type"
+    t.bigint "commentable_id"
+    t.integer "notation"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "report_sources", force: :cascade do |t|
@@ -93,6 +105,7 @@ ActiveRecord::Schema.define(version: 20170520155927) do
   end
 
   add_foreign_key "categories", "themes"
+  add_foreign_key "comments", "users"
   add_foreign_key "report_sources", "reports"
   add_foreign_key "reports", "tower_guards"
   add_foreign_key "reports", "towers"
