b39e9084a5df6785a64f641657f901b826e6b3e0
alexandre025 <alexandre.ferraille@gmail.com>
2017-05-25 12:06:57 +0200

    Add missing associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,12 @@
 class Category < ApplicationRecord
+
+  # Associations
+
   belongs_to :theme
+
+  has_many :towers
+
+  # Validators
 
   validates :theme, presence: true
   validates :name, uniqueness: { scope: :theme_id }
--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,5 +1,12 @@
 class Theme < ApplicationRecord
+
+  # Associations
+  
   has_many :categories
+
+  has_many :towers, through: :categories
+
+  # Validators
 
   validates :name, presence: true
 end
