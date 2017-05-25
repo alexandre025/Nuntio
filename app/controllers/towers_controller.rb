89f2589c38be5c057617018e0e32d18cd6574ee2
alexandre025 <alexandre.ferraille@gmail.com>
2017-05-25 12:07:53 +0200

    TowersController actions

--- app/controllers/towers_controller.rb
+++ app/controllers/towers_controller.rb
@@ -1,2 +1,16 @@
 class TowersController < ApplicationController
+
+  def index
+    @towers = Tower.all
+  end
+
+  def theme
+    @theme = Theme.find(params[:id])
+    @towers = @theme.towers
+  end
+
+  def show
+    @theme = Theme.find(params[:theme_id])
+    @tower = @theme.towers.find(params[:id])
+  end
 end
