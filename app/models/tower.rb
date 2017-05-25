69cda12f900421f0778579d8fc2817abc8b15bce
alexandre025 <alexandre.ferraille@gmail.com>
2017-05-25 12:07:35 +0200

    Clean up

--- app/models/tower.rb
+++ app/models/tower.rb
@@ -3,15 +3,21 @@ class Tower < ApplicationRecord
 
   monetize :price_per_month_cents
 
+  # Associations
+
   belongs_to :category
-
-  FREQUENCIES = %w(monthly).freeze
-
-  validates :category, :title, :description, presence: true
 
   has_one :tower_guard
   has_one :guard, through: :tower_guard
 
   # has_many :tower_guards
   # has_many :guards, through: :tower_guards
+
+  # Constants
+
+  FREQUENCIES = %w(monthly).freeze
+
+  # Validators
+
+  validates :category, :title, :description, presence: true
 end
