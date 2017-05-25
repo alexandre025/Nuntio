21e3930019004736767874077a58cb947c775037
alexandre025 <alexandre.ferraille@gmail.com>
2017-05-25 12:07:09 +0200

    Routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
   devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout' }
 
   get 'dashboard', to: 'dashboard#index'
+
+  get 'towers', to: 'towers#index'
+  get 'towers/:id', to: 'towers#theme', as: :theme_towers
+  get 'towers/:theme_id/:id', to: 'towers#show', as: :tower
+
 end
