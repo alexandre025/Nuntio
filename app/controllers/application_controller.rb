1760b519f9733cdb6f74f0a68751edff023f63a9
alexandre025 <alexandre.ferraille@gmail.com>
2017-05-31 12:47:09 +0200

    add raven context

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   layout Proc.new { |controller| controller.request.xhr? ? '/layouts/modal' : '/layouts/application' }
+
+  before_action :set_raven_context
+
+  private
+
+    def set_raven_context
+      Raven.user_context(id: current_user.id, email: current_user.email) if user_signed_in?
+      Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+    end
 end
