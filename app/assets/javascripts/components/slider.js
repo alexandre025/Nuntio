6121a3becfd259e3ebbca6209a8c32efc410d7f9
Maxime Guioneau <mguioneau@gmail.com>
2017-05-31 14:33:15 +0200

    Continue subheader

--- app/assets/javascripts/components/slider.js
+++ app/assets/javascripts/components/slider.js
@@ -1,14 +1,16 @@
 App.ready(function(){
 
   // TOWER LIST CAROUSEL
-  $(".tower-list__slider").slick({
-    infinite: false,
-    speed: 400,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    variableWidth: true,
-    nextArrow: '<div class="slick-arrow-next"></div>',
-    prevArrow: '<div class="slick-arrow-prev"></div>'
+  $(".tower-list__slider").each(function(){
+    $(this).slick({
+      infinite: false,
+      speed: 400,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      variableWidth: true,
+      nextArrow: '<div class="slick-arrow-next"></div>',
+      prevArrow: '<div class="slick-arrow-prev"></div>'
+    });
   });
 
 });
