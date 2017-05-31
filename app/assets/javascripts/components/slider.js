b1a8776a4db1c85d178f49da01cfa821ca0fbab8
Maxime Guioneau <mguioneau@gmail.com>
2017-05-31 14:50:44 +0200

    Add slider intialization verifictaion

--- app/assets/javascripts/components/slider.js
+++ app/assets/javascripts/components/slider.js
@@ -2,15 +2,17 @@ App.ready(function(){
 
   // TOWER LIST CAROUSEL
   $(".tower-list__slider").each(function(){
-    $(this).slick({
-      infinite: false,
-      speed: 400,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      variableWidth: true,
-      nextArrow: '<div class="slick-arrow-next"></div>',
-      prevArrow: '<div class="slick-arrow-prev"></div>'
-    });
+    if(!$(this).hasClass('slick-initialized')){
+      $(this).slick({
+        infinite: false,
+        speed: 400,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        variableWidth: true,
+        nextArrow: '<div class="slick-arrow-next"></div>',
+        prevArrow: '<div class="slick-arrow-prev"></div>'
+      });
+    }
   });
 
 });
