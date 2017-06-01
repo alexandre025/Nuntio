8a47ac3daabfb5f599dbbe4afb01be06c64e1c60
alexandre025 <alexandre.ferraille@gmail.com>
2017-06-01 09:45:19 +0200

    rubocop -a

--- db/migrate/20170601072325_add_fields_for_towers.rb
+++ db/migrate/20170601072325_add_fields_for_towers.rb
@@ -5,7 +5,7 @@ class AddFieldsForTowers < ActiveRecord::Migration[5.1]
     add_column :users, :firstname, :string, limit: 45
     add_column :users, :lastname, :string, limit: 45
 
-    Tower.update_all({grade: Tower::GRADES.first})
+    Tower.update_all(grade: Tower::GRADES.first)
 
     change_column :towers, :grade, :string, null: false, limit: 45
   end
