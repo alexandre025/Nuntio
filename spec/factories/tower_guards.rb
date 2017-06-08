42d272f8ffed5527b7daf34433899801e783cae3
alexandre025 <alexandre.ferraille@gmail.com>
2017-06-08 11:30:39 +0200

    update factories

--- spec/factories/tower_guards.rb
+++ spec/factories/tower_guards.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
   factory :tower_guard do
     guard
+    short_excerpt { Faker::Lorem.sentence }
+    excerpt { Faker::Lorem.paragraph(1) }
+    description { Faker::Lorem.paragraph(2) }
     association :tower, factory: [:tower, :with_category]
     roles [TowerGuard::ROLES.first]
   end
