9955017147411ff66b5a6a5ab1f30fa640ca1c85
alexandre025 <alexandre.ferraille@gmail.com>
2017-06-09 10:41:16 +0200

    work on tower / categories edit

--- app/admin/tower.rb
+++ app/admin/tower.rb
@@ -1,15 +1,43 @@
 ActiveAdmin.register Tower do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+  controller do
+    def find_resource
+      scoped_collection.friendly.find(params[:id])
+    end
+  end
 
+  index do
+    selectable_column
+    id_column
+    column :title
+    column :category do |tower|
+      link_to tower.category.name, admin_category_path(tower.category)
+    end
+    column :guard do |tower|
+      link_to tower.guard.fullname, admin_user_path(tower.guard)
+    end
+    column :excerpt
+    column :created_at
+    actions
+  end
+
+  filter :title
+  filter :category
+  filter :guard, collection: proc { TowerGuard.all.map{ |tg| [tg.guard.fullname, tg.guard.id]} }
+  filter :excerpt
+  filter :created_at
+
+  form do |f|
+    f.inputs "Admin Details" do
+      f.input :title
+      f.input :excerpt
+      f.input :short_excerpt
+      f.input :description
+      f.input :locales, collection: [:fr, :en, :de, :be]
+      f.input :price_per_month
+      f.input :frequency, collection: Tower::FREQUENCIES
+      f.input :category
+      f.input :grade, collection: Tower::GRADES
+    end
+    f.actions
+  end
 end
