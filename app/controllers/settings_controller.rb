43b01db0245ed7cac923f2903d0729d6395b9b68
alexandre025 <alexandre.ferraille@gmail.com>
2017-06-14 16:18:23 +0200

    rubocop -a

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -37,5 +37,4 @@ class SettingsController < ApplicationController
     def user_params
       params.require(:user).permit(:firstname, :lastname, :email, :password, :password_confirmation)
     end
-
 end
