b25661e9dd1a7848c77ddbb4fb17deb239a84cfb
Maxime Guioneau <mguioneau@gmail.com>
2017-06-17 11:19:17 +0200

    Beginning comments

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,8 +6,27 @@ class ReportsController < ApplicationController
 
     if current_user.subscribed_towers.include? @tower
       @report = @tower.reports.find(params[:id])
+      @comment = @report.comments.new
     else
       redirect_to dashboard_path
     end
   end
+
+  def comment
+    tower = Tower.friendly.find(params[:tower_id])
+    @report = tower.reports.find(params[:id])
+    @comment = tower.comments.new(comment_params)
+    if @comment.save
+      redirect_to tower_report(tower, @report), notice: 'Comment created'
+    else
+      render :show
+    end
+  end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
+
 end
